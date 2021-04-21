3fb1f3143bcbab964ddeaae2f9e0f8f750edaca3
Daniel Lane <dcevpersonal@gmail.com>
2021-04-21 18:10:25 +0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,8 @@ import "./index.css";
 import App from "./components/App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,4 +19,5 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorkerRegistration.register();
+reportWebVitals();
